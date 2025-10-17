ac0805f357263b9abb15daabcbbf53ed3827a268
Arnav2610 <arnavk2610@gmail.com>
2025-10-17 15:11:10 -0400

    Added js maps plugin to maps page + state selection option in cart page

--- cart/models.py
+++ cart/models.py
@@ -9,6 +9,7 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User,
         on_delete=models.CASCADE)
+    state = models.CharField(max_length=2, blank=True, null=True)
     def __str__(self):
         return str(self.id) + ' - ' + self.user.username
     
