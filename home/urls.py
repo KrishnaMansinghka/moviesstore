dfcb001b09770f87156aceee9b26d66bc047b3d3
Jessica Zhu <jzhu4012@gmail.com>
2025-10-16 22:13:59 -0400

    Update urls.py with local popularity map url
    [My computer is having issues with Django so I am manually uploading my changes]

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
+    path('local-popularity-map', views.local_popularity_map, name='home.local_popularity_map'),
 
 ]
